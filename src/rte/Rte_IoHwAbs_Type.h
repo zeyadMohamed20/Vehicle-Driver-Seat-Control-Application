65ea9e6f6bf7f054965ef4bd6d6459c9562a98c5
Zeyad Mohamed <1900959@eng.asu.edu.eg>
2024-12-09 16:31:36 +0200

    Use generated RTE to be compatible with SWC logic and build all the project

---
+++ src/rte/Rte_IoHwAbs_Type.h
@@ -0,0 +1,15 @@
+/**
+ *
+ * \brief Sprints AUTOSAR Master Class - SeatCtrlApp
+ * \author Zeyad Mohamed
+ *
+ * 
+ *
+ */
+ 
+#ifndef RTE_IOHWABS_TYPE_H
+#define RTE_IOHWABS_TYPE_H
+
+#include "Rte_Type.h"
+
+#endif
---
+++ src/rte/Rte_WeightSensor_Type.h
@@ -0,0 +1,15 @@
+/**
+ *
+ * \brief Sprints AUTOSAR Master Class - SeatCtrlApp
+ * \author Zeyad Mohamed
+ *
+ * 
+ *
+ */
+ 
+#ifndef RTE_WEIGHTSENSOR_TYPE_H
+#define RTE_WEIGHTSENSOR_TYPE_H
+
+#include "Rte_Type.h"
+
+#endif
